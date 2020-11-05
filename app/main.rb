4da6676a13f35b2e9ab4401392a813ebbba49735
marcoandre1 <marco-andre.echeverria.1@ulaval.ca>
2020-11-05 00:09:27 -0500

    functionnal game

--- app/main.rb
+++ app/main.rb
@@ -7,27 +7,38 @@ def tick args
 
   # ruby has an operator called ||= which means "only initialize this if it's nil"
   args.state.count_down   ||= 20 * 60 # set the count down to 20 seconds
+
+  # set the width and the height of the target
+  args.state.target_width = 100
+  args.state.target_height = 100
+
   # set the initial position of the target
-  args.state.target       ||= { x: args.grid.w.half,
-                                y: args.grid.h.half,
-                                w: 100,
-                                h: 100 }
+  args.state.target       ||= { x: args.grid.w.half - args.state.target_width / 2,
+                                y: args.grid.h.half - args.state.target_height / 2,
+                                w: args.state.target_width,
+                                h: args.state.target_height }
+
+  # set the width and the height of the player
+  args.state.player_width = 100
+  args.state.player_height = 100
 
   # set the initial position of the player
   args.state.player       ||= { x: 100,
                                 y: 100,
-                                w: 100,
-                                h: 100 }
+                                w: args.state.player_width,
+                                h: args.state.player_height }
 
   # set the player movement speed
   args.state.player_speed ||= 1
 
+  # set the player maxium speed
+  args.state.player_maximum_speed ||= 10
+
   # set the score
   args.state.score        ||= 0
-  args.state.teleports    ||= 10
 
   # set the instructions
-  args.state.instructions ||= "Get to the red goal! Use arrow keys to move. Spacebar to teleport (use them carefully)!"
+  args.state.instructions ||= "Get to the red goal! Use arrow keys to move."
 
   # sprite frame
   args.state.sprite_frame = args.state.tick_count.idiv(4).mod(6)
@@ -82,6 +93,18 @@ def tick args
                             h: args.state.target.h,
                             path: 'sprites/icon.png' }
 
+  # border specs
+  args.state.border_corner_x = 5
+  args.state.border_corner_y = 5
+  args.state.border_width = args.grid.w - 2 * args.state.border_corner_x
+  args.state.border_height = args.grid.h - 2 * args.state.border_corner_y
+
+  # render the border
+  args.outputs.borders << [ args.state.border_corner_x, 
+                            args.state.border_corner_y,
+                            args.state.border_width,
+                            args.state.border_height, 0, 0, 0]
+
   # ====================================================
   # run simulation
   # ====================================================
@@ -95,43 +118,40 @@ def tick args
   # ====================================================
   # if it isn't game over let them move
   if !game_over? args
-    # global variables
-    # dir_y = 0
-    # dir_x = 0
+
+    # collision with a wall will make the player bounce and increase maximum speed by one
+    if args.state.player.x < 0
+      args.state.dir_x *= -1
+      args.state.player_maximum_speed += 1
+    elsif args.state.player.x > args.grid.w - args.state.player_width
+      args.state.dir_x *= -1
+      args.state.player_maximum_speed += 1
+    end
+
+    # collision with a wall will make the player bounce and increase maximum speed by one
+    if args.state.player.y < 0
+      args.state.dir_y *= -1
+      args.state.player_maximum_speed += 1
+    elsif args.state.player.y > args.grid.h - args.state.player_height
+      args.state.dir_y *= -1
+      args.state.player_maximum_speed += 1
+    end
 
     # determine the change vertically
-    if args.inputs.keyboard.up
-    #  dir_y += args.state.player_speed
-      if args.state.dir_y < 10
-        args.state.dir_y += args.state.player_speed
-      end
-    elsif args.inputs.keyboard.down
-    #  dir_y -= args.state.player_speed
-      if args.state.dir_y > -10
-        args.state.dir_y -= args.state.player_speed
-      end
+    if args.inputs.keyboard.up && args.state.dir_y < args.state.player_maximum_speed
+      args.state.dir_y += args.state.player_speed
+    elsif args.inputs.keyboard.down && args.state.dir_y > -args.state.player_maximum_speed
+      args.state.dir_y -= args.state.player_speed
     end
 
     # determine the change horizontally
-    if args.inputs.keyboard.left
-    #  dir_x -= args.state.player_speed
-      if args.state.dir_x > -10
-        args.state.dir_x -= args.state.player_speed
-      end
-    elsif args.inputs.keyboard.right
-    #  dir_x += args.state.player_speed
-      if args.state.dir_x < 10
-        args.state.dir_x += args.state.player_speed
-      end
+    if args.inputs.keyboard.left && args.state.dir_x > -args.state.player_maximum_speed
+      args.state.dir_x -= args.state.player_speed
+    elsif args.inputs.keyboard.right && args.state.dir_x < args.state.player_maximum_speed
+      args.state.dir_x += args.state.player_speed
     end
 
-    # determine if teleport can be used
-    # if args.inputs.keyboard.key_down.space && args.state.teleports > 0
-    #  args.state.teleports -= 1
-    #  dir_x *= 20
-    #  dir_y *= 20
-    # end
-
+    # change the direction of the dragon according to his direction
     if args.state.dir_x < 0
       args.outputs.sprites << { x: args.state.player.x,
                                 y: args.state.player.y,
@@ -156,11 +176,21 @@ def tick args
     args.state.player.x += args.state.dir_x
     args.state.player.y += args.state.dir_y
   else
-    args.outputs.sprites << { x: args.state.player.x,
+    # keep the dragon flying in the good direction when game over
+    if args.state.dir_x < 0
+      args.outputs.sprites << { x: args.state.player.x,
+                                y: args.state.player.y,
+                                w: args.state.player.w,
+                                h: args.state.player.h,
+                                path: "sprites/dragon-left-#{args.state.sprite_frame}.png" }
+    else
+      args.outputs.sprites << { x: args.state.player.x,
                                 y: args.state.player.y,
                                 w: args.state.player.w,
                                 h: args.state.player.h,
                                 path: "sprites/dragon-right-#{args.state.sprite_frame}.png" }
+    end
+
     # if r is pressed, reset the game
     if args.inputs.keyboard.key_down.r
       $gtk.reset
@@ -181,20 +211,20 @@ def tick args
       args.state.score += 1
 
       # move the goal to a random location
-      args.state.target = { x: (rand args.grid.w), y: (rand args.grid.h), w: 100, h: 100 }
+      args.state.target = { x: (rand args.grid.w), y: (rand args.grid.h), w: args.state.target_width, h: args.state.target_height }
 
       # make sure the goal is inside the view area
       if args.state.target.x < 0
-        args.state.target.x += 20
-      elsif args.state.target.x > 1280
-        args.state.target.x -= 20
+        args.state.target.x += args.state.target_width
+      elsif args.state.target.x > args.grid.w - args.state.target_width
+        args.state.target.x -= args.state.target_width
       end
 
       # make sure the goal is inside the view area
       if args.state.target.y < 0
-        args.state.target.y += 20
-      elsif args.state.target.y > 720
-        args.state.target.y -= 20
+        args.state.target.y += args.state.target_height
+      elsif args.state.target.y > args.grid.h - args.state.target_height
+        args.state.target.y -= args.state.target_height
       end
     end
   end
